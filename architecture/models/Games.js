eb9d467d6ba2b626b01b503709d6d049cf89e0fc
John Stoklas <jstoklas@trinity.edu>
2025-04-26 16:46:42 -0500

    documentation for models

--- architecture/models/Games.js
+++ architecture/models/Games.js
@@ -3,13 +3,44 @@ const express = require('express');
 const mysql = require('mysql2/promise');
 const connection = require('../database').databaseConnection;
 
-class Games {
+/**
+ * @module models/Games
+ */
 
+/**
+ * @typedef {Object} Game
+ * @property {int} gameid
+ * @property {int} userid
+ * @property {int} topscore
+ * @property {int} topmoney
+ * @property {string} dateachieved
+ */
+
+class Games {
+    /**
+     * Adds a game to the database once the game is over.
+     * 
+     * @async
+     * @function addGame
+     * @memberof models/Games
+     * @param {int} userID 
+     * @param {int} score
+     * @param {int} money
+     * @returns {Promise<Void>} 
+    */
     static async addGame(userID, score, money) {
         const insertSQL = 'INSERT INTO games (userid, topscore, topmoney) VALUES (?, ?, ?)';
         return await connection.query(insertSQL, [userID, score, money]);
     }
 
+     /**
+     * Fetches all the scores in order from the database.
+     * 
+     * @async
+     * @function selectTopScores
+     * @memberof models/Games
+     * @returns {Promise<Array<Games>>} 
+    */
     static async selectTopScores() {
         const [games] = await connection.query(`
             SELECT g.gameid, g.userid, g.topscore, g.topmoney FROM games g
--- architecture/models/Traits.js
+++ architecture/models/Traits.js
@@ -3,8 +3,26 @@ const express = require('express');
 const mysql = require('mysql2/promise');
 const connection = require('../database').databaseConnection;
 
+/**
+ * @module models/Traits
+ */
+
+/**
+ * @typedef {Object} Trait
+ * @property {int} id
+ * @property {string} trait_name
+ * @property {int} goodtrait
+ */
+
 class Traits {
-    
+    /**
+     * Selects all possible traits from the database in order.
+     * 
+     * @async
+     * @function selectAllOrdered
+     * @memberof models/Traits
+     * @returns {Promise<Array<Traits>>} 
+    */
     static async selectAllOrdered() {
         const [traits] = await connection.query('SELECT * FROM traits ORDER BY trait_name');
         return traits;
